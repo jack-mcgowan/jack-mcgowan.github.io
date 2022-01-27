91c05c23ca9b687af1e5d9dcfef6b62d1014977d
Jack McGowan <jackmcgowan12@gmail.com>
2022-01-27 15:16:23 -0500

    Updating index, style, and js

--- js/main.js
+++ js/main.js
@@ -3,21 +3,8 @@ function date()
         let today = new Date();
         let time = today.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
         let date = today.toLocaleDateString('en-US', { dateStyle: 'long'} );
-        /*
-        var a = Date.now();
-        var b = new Date(a);
-        var time_options = {
-            hour: 'numeric',
-            minute: 'numeric',
-            hour12: true
-        };
-        var date_options = {
-            dateStyle: 'long'
-        }
-        var f = b.toLocaleTimeString('en-US', time_options)
-        var g = b.toLocaleDateString('en-US',date_options)
-        */
-        var timeString = time.toLowerCase()+" "+"|"+" "+date.toLowerCase()
+
+        var timeString = date.toLowerCase()+" "+"|"+" "+time.toLowerCase()
         document.getElementById("todaysDate").innerHTML = timeString;
     }
     setInterval(date,1000);
