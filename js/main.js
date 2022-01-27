4a1f2c1ab8ae0333a07b0a82ac88831f70a30a60
Jack McGowan <jackmcgowan12@gmail.com>
2022-01-27 15:01:12 -0500

    updated JS

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 function date() 
     {   
+        let today = new Date();
+        let time = today.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+        let date = today.toLocaleDateString('en-US', { dataStyle: 'long'} );
+        /*
         var a = Date.now();
         var b = new Date(a);
         var time_options = {
@@ -12,7 +16,8 @@ function date()
         }
         var f = b.toLocaleTimeString('en-US', time_options)
         var g = b.toLocaleDateString('en-US',date_options)
-        var timeString = g.toLowerCase()+" "+"|"+" "+f.toLowerCase()
+        */
+        var timeString = time.toLowerCase()+" "+"|"+" "+date.toLowerCase()
         document.getElementById("todaysDate").innerHTML = timeString;
     }
     setInterval(date,1000);
